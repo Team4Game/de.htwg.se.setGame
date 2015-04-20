1223fc940825db0d90c111bb10575b930e5c6530
rabertol <raina.bertolini@gmail.com>
2015-04-20 10:35:53 +0200

    #test start test covergage so that we can work with project

--- de.htwg.se.SetGame/test/de/htwg/se/setgame/model/impl/FieldTest.java
+++ de.htwg.se.SetGame/test/de/htwg/se/setgame/model/impl/FieldTest.java
@@ -1,17 +1,18 @@
 package de.htwg.se.setgame.model.impl;
 
-import static org.junit.Assert.*;
+
 
 import java.util.LinkedList;
 import java.util.List;
 import java.util.TreeSet;
 
-import junit.framework.Assert;
+
+import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
 
 import de.htwg.se.setgame.model.ICard;
-import de.htwg.se.setgame.model.impl.Field;
+
 
 public class FieldTest {
 	Field field;
@@ -25,10 +26,11 @@ public class FieldTest {
     @Test
     public void testStartUp_ok(){
         this.field.startUp();
-        org.junit.Assert.assertTrue(this.field.getAllCardsInGame() != null);
-        org.junit.Assert.assertEquals(this.field.getSizeofField(), this.field.getCardInFieldGame().size());
+        Assert.assertTrue(this.field.getAllCardsInGame() != null);
+        Assert.assertEquals(this.field.getSizeofField(), this.field.getCardInFieldGame().size());
         /*initial value of field*/
-        org.junit.Assert.assertTrue(this.field.getSizeofField() == this.field.INITIALVALUEOFFIELD);
+        Assert.assertTrue(this.field.getSizeofField() == this.field.INITIALVALUEOFFIELD);
+        Assert.assertTrue(field.getAllCardsInGame().size() == 81);
     }
 
 	@Test
@@ -36,7 +38,7 @@ public class FieldTest {
         this.field.startUp();
 		TreeSet<Integer> liste = new TreeSet<Integer>();
 		liste.addAll(this.field.getRamdomListe().values());
-        org.junit.Assert.assertTrue(liste.size() == 81);
+        Assert.assertTrue(liste.size() == 81);
 	}
 
 	@Test
@@ -49,14 +51,14 @@ public class FieldTest {
 					liste.get((index+1)),
 					liste.get(index+2));
 		}
-        org.junit.Assert.assertTrue(this.field.getAllCardsInGame().isEmpty());
+        Assert.assertTrue(this.field.getAllCardsInGame().isEmpty());
 	}
 
 	@Test
 	public void testCardsInField() {
         this.field.startUp();
         List<ICard> cardInField = this.field.getCardsInField();
-        org.junit.Assert.assertTrue(this.field.getCardInFieldGame().values().containsAll(this.field.getCardsInField()));
+        Assert.assertTrue(this.field.getCardInFieldGame().values().containsAll(this.field.getCardsInField()));
 
 	}
     @Test
@@ -64,23 +66,19 @@ public class FieldTest {
         this.field.startUp();
         this.field.setSizeOfField(50);
         this.field.clear();
-        org.junit.Assert.assertTrue(this.field.getAllCardsInGame() != null);
-        org.junit.Assert.assertEquals(this.field.getSizeofField(), this.field.getCardInFieldGame().size());
+        Assert.assertTrue(this.field.getAllCardsInGame() != null);
+        Assert.assertEquals(this.field.getSizeofField(), this.field.getCardInFieldGame().size());
         /*initial value of field*/
-        org.junit.Assert.assertTrue(this.field.getSizeofField() == this.field.INITIALVALUEOFFIELD);
+        Assert.assertTrue(this.field.getSizeofField() == this.field.INITIALVALUEOFFIELD);
     }
 
 	@Test
 	public void testSetSizeOfField() {
         this.field.startUp();
-		LinkedList<ICard> list = new LinkedList<ICard>();
-		list.add(this.field.getCardsInField().get(0));
-		list.add(this.field.getCardInFieldGame().get(1));
-		list.add(this.field.getCardInFieldGame().get(2));
 		this.field.setSizeOfField(15);
-        org.junit.Assert.assertTrue(this.field.getCardsInField().size() == 15);
+        Assert.assertTrue(this.field.getCardsInField().size() == 15);
 		this.field.setSizeOfField(12);
-        org.junit.Assert.assertTrue(this.field.getCardsInField().size() == 12);
+        Assert.assertTrue(this.field.getCardsInField().size() == 12);
 	}
  
 	@Test
@@ -91,23 +89,14 @@ public class FieldTest {
 		liste.add(this.field.getUnusedCards().get(0));
 		liste.add(this.field.getUnusedCards().get(1));
 		this.field.changeCards(liste);
+        Assert.assertTrue(this.field.getCardInFieldGame().values().containsAll(liste));
 
 
 	}
-
-	@Test
-	public void testGetPackForControler() {
+    @Test
+    public void testTostring(){
         this.field.startUp();
-		if(field.getAllCardsInGame().size() != 81){
-			fail("feld gr��e stimmt nicht");
-		}
-	}
-
-	@Test
-	public void testGetSizeofField() {
-        this.field.startUp();
-		if(this.field.getSizeofField() != 12){
-			fail("Feld an anfagen muss 12 sein!!!");
-		}
-	}
+       String field =  this.field.toString();
+        Assert.assertTrue(field != null);
+    }
 }
