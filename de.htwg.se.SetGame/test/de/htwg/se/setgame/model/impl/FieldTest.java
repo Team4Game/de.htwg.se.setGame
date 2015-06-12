6f21ee29371769c63c3659de17df6df5ffe19a11
rabertol <raina.bertolini@gmail.com>
2015-06-12 15:41:24 +0200

    test for field

--- de.htwg.se.SetGame/test/de/htwg/se/setgame/model/impl/FieldTest.java
+++ de.htwg.se.SetGame/test/de/htwg/se/setgame/model/impl/FieldTest.java
@@ -2,23 +2,29 @@ package de.htwg.se.setgame.model.impl;
 
 
 
+import de.htwg.se.setgame.model.ICard;
 import de.htwg.se.setgame.model.IField;
 import de.htwg.se.setgame.model.IModelFactory;
 import org.junit.Before;
 import org.junit.Test;
 
 
+import java.util.LinkedList;
+import java.util.List;
+
+
 public class FieldTest {
 	IField target;
-
+    IModelFactory modelFactory;
 	@Before
 	public void setUp() {
-        IModelFactory modelFactory = new ModelFactory();
+        modelFactory = new ModelFactory();
 		this.target = modelFactory.createField();
 	}
     @Test
     public void setFieldCards(){
-
+        List<ICard> list = new LinkedList<ICard>();
+        
     }
 
 
