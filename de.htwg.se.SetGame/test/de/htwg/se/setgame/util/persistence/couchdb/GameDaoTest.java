5458bc6e04d882a87b6aaa006dd7f72e38656a99
rabertol <raina.bertolini@gmail.com>
2015-06-16 14:28:56 +0200

    couchdbTest

---
+++ de.htwg.se.SetGame/test/de/htwg/se/setgame/util/persistence/couchdb/GameDaoTest.java
@@ -0,0 +1,17 @@
+package de.htwg.se.setgame.util.persistence.couchdb;
+
+import org.junit.Before;
+import org.junit.Test;
+
+/**
+ * Created by raina on 16.06.2015.
+ */
+public class GameDaoTest {
+    GameDao target;
+
+    @Before
+    public void setUp(){
+        target = new GameDao();
+    }
+
+}
