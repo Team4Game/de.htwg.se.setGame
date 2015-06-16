4e68aaa5bc1fdca8512b7039cebd999ae176ad15
rabertol <raina.bertolini@gmail.com>
2015-06-16 14:28:06 +0200

    couchdg

--- de.htwg.se.SetGame/src/de/htwg/se/setgame/util/persistence/couchdb/PersistentCard.java
+++ de.htwg.se.SetGame/src/de/htwg/se/setgame/util/persistence/couchdb/PersistentCard.java
@@ -12,39 +12,24 @@ import de.htwg.se.setgame.model.impl.atributte.CardAtributen;
 
 public class PersistentCard extends CouchDbDocument implements ICard {
 	/**
-	 * 
+	 *
 	 */
 	private static final long serialVersionUID = 9000465800577475080L;
-	
+
 	private String color;
 	private String form;
 	private String panelFilling;
 	private int anz;
 
 	public PersistentCard() {};
-	
-	/**
-	 * This method create a new card object
-	 * 
-	 * @param color
-	 * @param form
-	 * @param panelFilling
-	 * @param amount
-	 */
-	public PersistentCard(String color, String form, String panelFilling, int amount) {
-		this.setColor(color);
-		this.setForm(form);
-		this.setPanelFilling(panelFilling);
-		this.setNumberOfComponents(amount);
-
-	}
 
 	/*
 	 * (non-Javadoc)
-	 * 
+	 *
 	 * @see de.htwg.se.setgame.modell.impl.ICard#getColor()
 	 */
-	public String getColor() {
+	@Override
+    public String getColor() {
 		return color;
 	}
 
@@ -52,6 +37,7 @@ public class PersistentCard extends CouchDbDocument implements ICard {
 	 * @param color
 	 *            set the color of a card
 	 */
+    @Override
 	public void setColor(String color) {
 		if (color.equals(CardAtributen.COLORS[0]) || color.equals(CardAtributen.COLORS[1])
 				|| color.equals(CardAtributen.COLORS[2])) {
@@ -62,11 +48,7 @@ public class PersistentCard extends CouchDbDocument implements ICard {
 
 	}
 
-	/*
-	 * (non-Javadoc)
-	 * 
-	 * @see de.htwg.se.setgame.modell.impl.ICard#getFomr()
-	 */
+	@Override
 	public String getForm() {
 		return form;
 	}
@@ -77,7 +59,8 @@ public class PersistentCard extends CouchDbDocument implements ICard {
 	 * @throws IllegalArgumentException
 	 *             if the form is not allowed
 	 */
-	public void setForm(String form) {
+	@Override
+    public void setForm(String form) {
 		if (form.equals(CardAtributen.FORME[2]) || form.equals(CardAtributen.FORME[0])
 				|| form.equals(CardAtributen.FORME[1])) {
 			this.form = form;
@@ -88,7 +71,7 @@ public class PersistentCard extends CouchDbDocument implements ICard {
 
 	/*
 	 * (non-Javadoc)
-	 * 
+	 *
 	 * @see de.htwg.se.setgame.modell.impl.ICard#getPanelFilling()
 	 */
 	public String getPanelFilling() {
@@ -114,7 +97,7 @@ public class PersistentCard extends CouchDbDocument implements ICard {
 
 	/*
 	 * (non-Javadoc)
-	 * 
+	 *
 	 * @see de.htwg.se.setgame.modell.impl.ICard#getNumberOfComponents()
 	 */
 	public int getNumberOfComponents() {
@@ -139,9 +122,10 @@ public class PersistentCard extends CouchDbDocument implements ICard {
 
 	/*
 	 * (non-Javadoc)
-	 * 
+	 *
 	 * @see de.htwg.se.setgame.modell.impl.ICard#toString()
 	 */
+    @Override
 	public String toString() {
 		StringBuilder sb = new StringBuilder();
 		sb.append("|" + this.color + "| \n " + "|" + this.form + "| \n " + "|"
@@ -150,7 +134,7 @@ public class PersistentCard extends CouchDbDocument implements ICard {
 	}
 
 
-
+    @Override
 	public boolean compareTo(ICard card) {
 
 		if (this.color.equals(card.getColor()) && this.anz == card.getNumberOfComponents()
