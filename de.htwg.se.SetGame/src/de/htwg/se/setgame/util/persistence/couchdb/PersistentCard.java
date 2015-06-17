00c90f5cd866d8095c71e956edcf0ecfb9407dc9
rabertol <raina.bertolini@gmail.com>
2015-06-17 16:17:50 +0200

    violation

--- de.htwg.se.SetGame/src/de/htwg/se/setgame/util/persistence/couchdb/PersistentCard.java
+++ de.htwg.se.SetGame/src/de/htwg/se/setgame/util/persistence/couchdb/PersistentCard.java
@@ -1,17 +1,7 @@
 package de.htwg.se.setgame.util.persistence.couchdb;
 
-import com.db4o.User;
-import de.htwg.se.setgame.model.ICard;
 import de.htwg.se.setgame.model.impl.Card;
-import org.codehaus.jackson.JsonNode;
-import org.codehaus.jackson.JsonParser;
-import org.codehaus.jackson.JsonProcessingException;
-import org.codehaus.jackson.ObjectCodec;
 import org.codehaus.jackson.annotate.JsonProperty;
-import org.codehaus.jackson.map.DeserializationContext;
-import org.codehaus.jackson.map.JsonDeserializer;
-
-import java.io.IOException;
 
 /**
  * @author David Simon & Raina Bertolini
@@ -41,16 +31,5 @@ public class PersistentCard extends Card {
         this.dbRevCard = dbRevCard;
     }
 
-/*    @Override
-    public Card deserialize(JsonParser jp, DeserializationContext ctxt) throws IOException, JsonProcessingException {
-        ObjectCodec oc = jp.getCodec();
-        JsonNode node = oc.readTree(jp);
-        ICard result = new Card();
-        result.setColor(node.get("color").getTextValue());
-        result.setColor(node.get("form").getTextValue());
-        result.setColor(node.get("panelFilling").getTextValue());
-        result.setNumberOfComponents();
 
-        return result;
-    }*/
 }
