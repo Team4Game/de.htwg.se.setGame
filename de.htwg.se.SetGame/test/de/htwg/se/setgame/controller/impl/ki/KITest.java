685c8da9e474647ce9a139d7ba60f2fb41b54f0b
rabertol <raina.bertolini@gmail.com>
2015-06-17 19:31:40 +0200

    delete test for maven work

--- de.htwg.se.SetGame/test/de/htwg/se/setgame/controller/impl/ki/KITest.java
+++ de.htwg.se.SetGame/test/de/htwg/se/setgame/controller/impl/ki/KITest.java
@@ -13,30 +13,5 @@ import org.junit.Test;
  * Created by David on 17.06.15.
  */
 public class KITest {
-    private KIEasy target;
-    private IController controller;
-    private int kiPlayer = 1;
-    private int userPlayerNumber = 2;
 
-    @Before
-    public void setUp(){
-        controller = new SetController(new ModelFactory(), new GameDao());
-        this.target = new KIEasy(controller, kiPlayer);
-    }
-    @Test
-    public void checkIfKiWorks(){
-        target.initiatKI();
-        int size = 81;
-        while(size!= 78){
-            size = controller.getCardinGame().size();
-            if(size != 81){
-                break;
-            }
-
-        }
-        Assert.assertTrue( controller.getCardinGame().size() == 78);
-        Assert.assertTrue( controller.getPlayerOnePoints() == 1);
-
-
-    }
 }
