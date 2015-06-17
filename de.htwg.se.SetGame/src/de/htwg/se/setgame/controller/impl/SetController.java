90ee04f49a7b9b8380c01fe593d5dca996914f68
rabertol <raina.bertolini@gmail.com>
2015-06-17 23:34:54 +0200

    insert is a set service

--- de.htwg.se.SetGame/src/de/htwg/se/setgame/controller/impl/SetController.java
+++ de.htwg.se.SetGame/src/de/htwg/se/setgame/controller/impl/SetController.java
@@ -170,94 +170,16 @@ public class SetController extends Observable implements IController {
 
 	}
 
-	/**
-	 * @param cardOne
-	 * @param cardTwo
-	 * @param cardThree
-	 * @return
-	 */
-	private boolean proveColor(ICard cardOne, ICard cardTwo, ICard cardThree) {
-		return proveString(cardOne.getColor(), cardTwo.getColor(),
-				cardThree.getColor());
-	}
 
-	/**
-	 * @param cardOne
-	 * @param cardTwo
-	 * @param cardThree
-	 * @return
-	 */
-	private boolean proveNumberOfComponents(ICard cardOne, ICard cardTwo,
-			ICard cardThree) {
-		if (cardOne.getNumberOfComponents() == cardTwo.getNumberOfComponents()
-				&& cardOne.getNumberOfComponents() == cardThree
-						.getNumberOfComponents()) {
-			return true;
-		} else if (cardOne.getNumberOfComponents() != cardTwo
-				.getNumberOfComponents()
-				&& cardOne.getNumberOfComponents() != cardThree
-						.getNumberOfComponents()
-				&& cardTwo.getNumberOfComponents() != cardThree
-						.getNumberOfComponents()) {
-			return true;
-		}
-		return false;
-	}
-
-	/**
-	 * @param cardOne
-	 * @param cardTwo
-	 * @param cardThree
-	 * @return
-	 */
-	private boolean proveFilling(ICard cardOne, ICard cardTwo, ICard cardThree) {
-		return proveString(cardOne.getPanelFilling(),
-				cardTwo.getPanelFilling(), cardThree.getPanelFilling());
-	}
-
-	/**
-	 * @param cardOne
-	 * @param cardTwo
-	 * @param cardThree
-	 * @return
-	 */
-	private boolean proveForm(ICard cardOne, ICard cardTwo, ICard cardThree) {
-		return proveString(cardOne.getForm(), cardTwo.getForm(),
-				cardThree.getForm());
-	}
-
-	/**
-	 * @param stringOne
-	 * @param stringTwo
-	 * @param stringThree
-	 * @return
-	 */
-	private boolean proveString(String stringOne, String stringTwo,
-			String stringThree) {
-		if (stringOne.compareTo(stringTwo) == 0
-				&& stringOne.compareTo(stringThree) == 0
-				&& stringTwo.compareTo(stringThree) == 0) {
-			return true;
-		} else if (stringOne.compareTo(stringTwo) != 0
-				&& stringOne.compareTo(stringThree) != 0
-				&& stringTwo.compareTo(stringThree) != 0) {
-			return true;
-		}
-		return false;
-	}
-
-	/**
-	 * @param cardOne
-	 * @param cardTwo
-	 * @param cardThree
-	 * @return
-	 */
+    /**
+     * @param cardOne
+     * @param cardTwo
+     * @param cardThree
+     * @return
+     */
 	private boolean proveIfIsASet(ICard cardOne, ICard cardTwo, ICard cardThree) {
-		if (proveColor(cardOne, cardTwo, cardThree)
-				&& proveFilling(cardOne, cardTwo, cardThree)
-				&& proveNumberOfComponents(cardOne, cardTwo, cardThree)
-				&& proveForm(cardOne, cardTwo, cardThree)) {
-			return true;
+        if (setService.isASet(cardOne, cardTwo, cardThree)) {
+            return true;
 		}
 		return false;
 
--- de.htwg.se.SetGame/src/de/htwg/se/setgame/controller/impl/service/SetService.java
+++ de.htwg.se.SetGame/src/de/htwg/se/setgame/controller/impl/service/SetService.java
@@ -1,7 +1,98 @@
 package de.htwg.se.setgame.controller.impl.service;
 
+import de.htwg.se.setgame.model.ICard;
+
 /**
  * Created by raina on 17.06.2015.
  */
 public class SetService {
+
+    public SetService(){
+
+    }
+    /**
+     * @param stringOne color
+     * @param stringTwo form
+     * @param stringThree filling
+     * @return
+     */
+    public boolean proveString(String stringOne, String stringTwo,
+                                String stringThree) {
+        if (stringOne.compareTo(stringTwo) == 0
+                && stringOne.compareTo(stringThree) == 0
+                && stringTwo.compareTo(stringThree) == 0) {
+            return true;
+        } else if (stringOne.compareTo(stringTwo) != 0
+                && stringOne.compareTo(stringThree) != 0
+                && stringTwo.compareTo(stringThree) != 0) {
+            return true;
+        }
+        return false;
+    }
+    /**
+     * @param cardOne
+     * @param cardTwo
+     * @param cardThree
+     * @return
+     */
+    public boolean proveNumberOfComponents(ICard cardOne, ICard cardTwo,
+                                            ICard cardThree) {
+        if (cardOne.getNumberOfComponents() == cardTwo.getNumberOfComponents()
+                && cardOne.getNumberOfComponents() == cardThree
+                .getNumberOfComponents()) {
+            return true;
+        } else if (cardOne.getNumberOfComponents() != cardTwo
+                .getNumberOfComponents()
+                && cardOne.getNumberOfComponents() != cardThree
+                .getNumberOfComponents()
+                && cardTwo.getNumberOfComponents() != cardThree
+                .getNumberOfComponents()) {
+            return true;
+        }
+        return false;
+    }
+    /**
+     * @param cardOne
+     * @param cardTwo
+     * @param cardThree
+     * @return
+     */
+    public boolean proveForm(ICard cardOne, ICard cardTwo, ICard cardThree) {
+        return proveString(cardOne.getForm(), cardTwo.getForm(),
+                cardThree.getForm());
+    }
+
+    public boolean isASet(ICard cardOne, ICard cardTwo, ICard cardThree) {
+        if(proveColor(cardOne, cardTwo, cardThree)
+                && proveFilling(cardOne, cardTwo, cardThree)
+                && proveNumberOfComponents(cardOne, cardTwo, cardThree)
+                && proveForm(cardOne, cardTwo, cardThree)){
+            return true;
+        }
+        return false;
+    }
+
+    /**
+     * @param cardOne
+     * @param cardTwo
+     * @param cardThree
+     * @return
+     */
+    private boolean proveColor(ICard cardOne, ICard cardTwo, ICard cardThree) {
+        return proveString(cardOne.getColor(), cardTwo.getColor(),
+                cardThree.getColor());
+    }
+
+
+
+    /**
+     * @param cardOne
+     * @param cardTwo
+     * @param cardThree
+     * @return
+     */
+    private boolean proveFilling(ICard cardOne, ICard cardTwo, ICard cardThree) {
+        return proveString(cardOne.getPanelFilling(),
+                cardTwo.getPanelFilling(), cardThree.getPanelFilling());
+    }
 }
