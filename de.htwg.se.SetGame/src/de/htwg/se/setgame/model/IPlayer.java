70f1b6c6d58b00f4755757bc1f7168eac8c82865
David Rotaermel <dr93@hotmail.de>
2016-01-11 11:08:53 +0100

    Model IPlayer Interface

--- de.htwg.se.SetGame/src/de/htwg/se/setgame/model/IPlayer.java
+++ de.htwg.se.SetGame/src/de/htwg/se/setgame/model/IPlayer.java
@@ -1,8 +1,13 @@
 package de.htwg.se.setgame.model;
 
+
+import de.htwg.se.setgame.util.persistance.couchDB.pojo.Player;
+import org.codehaus.jackson.map.annotate.JsonDeserialize;
+
 /**
  * Created by raina on 03.06.2015.
  */
+@JsonDeserialize(as = Player.class)
 public interface IPlayer {
 	
 	void setId(int id);
